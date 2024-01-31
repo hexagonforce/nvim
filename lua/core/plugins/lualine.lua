c81bf7c7ee67d0ec1fe5da00f909845951a329d2
Hyeong Seon Yoo (유형선) <sylvesteryoo@naver.com>
2024-01-31 09:16:36 +0900

    change lualine to show buffers and tabs

--- lua/core/plugins/lualine.lua
+++ lua/core/plugins/lualine.lua
@@ -1,6 +1,7 @@
 return {
   'nvim-lualine/lualine.nvim',
   lazy = false,
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
     require("lualine").setup(
     {
@@ -13,11 +14,20 @@ return {
       },
       sections = {
         lualine_a = {
+          'mode'
+        },
+        lualine_c = {
           {
             'filename',
             path = 1,
           }
         }
+      },
+      tabline = {
+        lualine_a = {'buffers'},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {'tabs'}
       }
     })
   end
