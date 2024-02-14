9d2c3db30145c092b815ca73e79ee818e99deb2d
Hyeong Seon Yoo (유형선) <sylvesteryoo@naver.com>
2024-02-14 21:14:16 +0900

    removed nvim-tree so that I'm using netrw instead

--- lua/core/plugins/lspconfig.lua
+++ lua/core/plugins/lspconfig.lua
@@ -1,17 +1,7 @@
 return {
   'neovim/nvim-lspconfig',
   config = function()
-    require('lspconfig').pylsp.setup{
-      settings = {
-        pylsp = {
-          plugins = {
-            pycodestyle = {
-              ignore = {'W391'},
-              maxLineLength = 100
-            }
-          }
-        }
-      }
-    }
+    require('lspconfig').pyright.setup{}
+    require('lspconfig').clangd.setup{}
   end
 }
