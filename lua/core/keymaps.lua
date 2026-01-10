1c358dd6e044597c7bf1a57c836106431499785c
Hyeong Seon Yoo (유형선) <sylvesteryoo@naver.com>
2026-01-10 21:41:32 +0900

    configure LSP properly

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -10,6 +10,12 @@ vim.keymap.set('n', '<leader>hs', ':split<CR>')
 vim.keymap.set('n', '<leader>ss', ':mks! ~/session.vim<CR>')
 vim.keymap.set('n', '<leader>so', ':source ~/session.vim<CR>')
 vim.keymap.set('n', '<leader>ee', ':NvimTreeFindFileToggle<CR>')
+vim.keymap.set('n', '<leader>ef', ':NvimTreeFindFile<CR>')
+
+vim.api.nvim_set_keymap('n', '<leader>do', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>d[', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>d]', '<cmd>lua vim.diagnostic.goto_next()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>dd', '<cmd>Telescope diagnostics<CR>', { noremap = true, silent = true })
 
 vim.keymap.set('n', '<leader>src', ':source $MYVIMRC<CR>')
 vim.keymap.set('n', '<leader>vi', ':tabnew ~/.config/nvim<CR>')
--- lua/core/plugins/treesitter.lua
+++ lua/core/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
   'nvim-treesitter/nvim-treesitter',
   config = function()
     require('nvim-treesitter.configs').setup({
-      ensure_installed = {"c", "lua", "python", "javascript", "rust", "gitignore"},
+      ensure_installed = {"c", "lua", "python", "javascript", "rust", "gitignore", "markdown"},
       sync_install = false,
       auto_install = true,
       highlight = {
